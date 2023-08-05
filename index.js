44da043b6fac8a4f76beb32411b690a35b6c3779
hackerslash <afridijnv19@gmail.com>
2023-08-06 00:27:59 +0530

    final build

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
+app.use(express.static('build'))
 
 // morgan configuration
 morgan.token("body", (req, res) => JSON.stringify(req.body));
@@ -80,13 +82,7 @@ app.post("/api/persons", (request, response) => {
   }
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
-
-const PORT = 3001;
+const PORT = 3001 || process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
